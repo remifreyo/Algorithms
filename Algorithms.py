67b809c0968e65a29ad6354d053fb3185a60a24e
Jeremy Cox <jeremy@theoutlet.org>
2023-09-10 18:44:13 -0400

    add bubble and insertion sort

--- Algorithms.py
+++ Algorithms.py
@@ -86,18 +86,18 @@ def count_rotations_binary(nums):
 #Function for Evaluating Test Cases
 def evaluate_test_cases(function, tests):
     for test in tests:
-      t.start()
-      if function == locate_card_linear or function == locate_card_binary:
-          result = function(test['input']['cards'], test['input']['query'])
-      elif function == count_rotations_linear or function == count_rotations_binary:
-          result = function(test['input']['nums'])
-      print('Function:', function)
-      print('Input:', test['input'])
-      print('Expected Output:', test['output'])
-      print('Actual Output:', result)
-      print('Test Passed:', (result == test['output']))
-      t.stop()
-      print('')
+        t.start()
+        if function == locate_card_linear or function == locate_card_binary:
+            result = function(test['input']['cards'], test['input']['query'])
+        elif function == count_rotations_linear or function == count_rotations_binary or function == bubble_sort or function == insertion_sort:
+            result = function(test['input']['nums'])
+        print('Function:', function)
+        print('Input:', test['input'])
+        print('Expected Output:', test['output'])
+        print('Actual Output:', result)
+        print('Test Passed:', (result == test['output']))
+        t.stop()
+        print('')
 
 #Test Cases
 tests = [
@@ -112,7 +112,7 @@ tests = [
     {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 6}, 'output': 2}
     ]
 
-new_tests = [
+tests2 = [
     {'input': {'nums': [19, 25, 29, 3, 5, 6, 7, 9, 11, 14]}, 'output': 3}, 
     {'input': {'nums': [0]}, 'output': 0},
     {'input': {'nums': [5, 6, 7, 1, 3]}, 'output': 3},
@@ -121,8 +121,8 @@ new_tests = [
 
 # evaluate_test_cases(locate_card_linear, tests)
 # evaluate_test_cases(locate_card_binary, tests)
-# evaluate_test_cases(count_rotations_linear, new_tests)
-# evaluate_test_cases(count_rotations_binary, new_tests)
+# evaluate_test_cases(count_rotations_linear, tests2)
+# evaluate_test_cases(count_rotations_binary, tests2)
 
 #Python Classes 
 # Insert, Find, Update, and List (Brute Force/Linear)
@@ -361,12 +361,93 @@ table = HashTable()
 table.insert('Jeremy', '3811675')
 
 #Sorting
-def sort(nums):
-    pass
+def bubble_sort(nums):
+    nums = list(nums)
+    for _ in range(len(nums)-1):
+        for i in range(len(nums) -1):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+    return nums
 
+def insertion_sort(nums):
+    nums = list(nums)
+    for i in range(len(nums)):
+        cur = nums.pop(i)
+        j = i-1
+        while j >=0 and nums[j] > cur:
+            j -=1
+        nums.insert(j+1, cur)
+    return nums
 
-print((
+#Test Cases
+#List of numbers in random order
+test0 = {
+    'input': {
+        'nums': [4,2,6,3,4,6,2,1]
+    },
+    'output': [1,2,2,3,4,4,6,6]
+}
+#List of numbers in random order
+test1 = {
+    'input': {
+        'nums': [5,2,6,1,23,7,-12,12,-243,0]
+    },
+    'output': [-243,-12,0,1,2,5,6,7,12,23]
+}
+#List of numbers already sorted
+test2 = {
+    'input': {
+        'nums': [3,5,6,8,9,10,99]
+    },
+    'output': [3,5,6,8,9,10,99]
+}
+#List of numbers in descending order
+test3 = {
+    'input': {
+        'nums': [99,10,9,8,6,5,3]
+    },
+    'output': [3,5,6,8,9,10,99]
+}
+#List containing repeating elements
+test4 = {
+    'input': {
+        'nums': [5,-12,2,6,1,23,7,7,-12,6,12,1,-243,1,0]
+    },
+    'output': [-243,-12,-12,0,1,1,1,2,5,6,6,7,7,12,23]
+}
+#An Empty List
+test5 = {
+    'input': {
+        'nums': []
+    },
+    'output': []
+}
+#A list Containing just one element
+test6 = {
+    'input': {
+        'nums': [23]
+    },
+    'output': [23]
+}
+#A list containing one elemenet repeated many times
+test7 = {
+    'input': {
+        'nums': [42,42,42,42,42,42,42,42]
+    },
+    'output': [42,42,42,42,42,42,42,42]
+}
+#A really long list
+import random
+in_list = list(range(10000))
+out_list = list(range(10000))
+random.shuffle(in_list)
+test8 = {
+    'input': {
+        'nums': in_list
+    },
+    'output': out_list
+}
+tests3 = [test0, test1, test2, test3, test4, test5, test6, test7, test8]
 
-
-
-))
+# evaluate_test_cases(bubble_sort, tests3)
+# evaluate_test_cases(insertion_sort, tests3)
